cb053a255ebf7c32c8669c078304262697ebea9a
Harsh Choubey <harshchoubey712@gmail.com>
2025-12-07 23:38:31 +0100

    db changes

--- src/config/database1.js
+++ src/config/database1.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose.connect(
     // "mongodb://harshchoubey712:mXbnTNgcHAfDzKM6@ac-zggtaag-shard-00-00.zowkghs.mongodb.net:27017,ac-zggtaag-shard-00-01.zowkghs.mongodb.net:27017,ac-zggtaag-shard-00-02.zowkghs.mongodb.net:27017/?replicaSet=atlas-92z3cp-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=NamasteNode"
-    "mongodb+srv://harshchoubey712:mXbnTNgcHAfDzKM6@namastenode.zowkghs.mongodb.net/devTinder"
+    "mongodb+srv://harshchoubey712:cwoi4P1W8ku0t3NV@namastenode.zowkghs.mongodb.net/devTinder"
   );
 
   //mongodb+srv://harshchoubey712:mXbnTNgcHAfDzKM6@namastenode.zowkghs.mongodb.net/?appName=NamasteNode
